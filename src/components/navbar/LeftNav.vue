b612979b0829848cf6f3830afe9ffaa5c227ca09
aldi <aldipranataal22@gmail.com>
2024-05-06 22:33:15 +0700

    first commit

---
+++ src/components/navbar/LeftNav.vue
@@ -0,0 +1,19 @@
+<template>
+        <v-navigation-drawer
+          expand-on-hover
+          rail
+          color="grey"
+        >
+   
+          <v-divider></v-divider>
+  
+          <v-list density="compact" nav>
+            <v-list-item prepend-icon="mdi-folder" title="Home" :to="'/home'"></v-list-item>
+            <v-list-item prepend-icon="mdi-account-multiple" title="Dosen" :to="'/dosen-entry'"></v-list-item>
+            <v-list-item prepend-icon="mdi-star" title="Mahasiswa" :to="'/mahasiswa-entry'"></v-list-item>
+            <v-list-item prepend-icon="mdi-star" title="Ruangan" :to="'/ruangan-entry'"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+  
+        <v-main style="height: 250px"></v-main>
+  </template>
---
+++ src/views/uts/home.vue
@@ -0,0 +1,9 @@
+<template>
+<h1>SELAMAT DATANG DI APLIKASI</h1>
+</br>
+<h2>Aplikasi ini dibuat oleh : </h2>
+</br>
+<h4>Nama : Aldi Pranata Kusuma</h4>
+</br>
+<h4>NPM : 40622100096</h4><br>
+</template>
